9ca5c424867b3fbfd97d3968b5d67bd73eb7079b
Fabian Camayo <famunoz@knowark.com>
2021-12-11 18:35:19 -0500

    add status in models

--- example/server/tutorark/application/domain/models/lesson.py
+++ example/server/tutorark/application/domain/models/lesson.py
@@ -6,3 +6,36 @@ class Lesson(Entity):
         super().__init__(**attributes)
         self.course_id = attributes['course_id']
         self.name = attributes['name']
+
+    @property
+    def transitions(self):
+        return TRANSITIONS_MAP[self.status]
+
+    def transition(self, state: dict):
+        if state.get('status'):
+
+            target = self.transitions.get(state.get('status'), {})
+            if target.get('name') == "start":
+                self.__dict__.update(state)
+
+            if target.get('name') == "finalize":
+                self.__dict__.update(state)
+
+        self.__dict__.update(state)
+
+        return self
+
+TRANSITIONS_MAP = {
+    "":{
+        "initiated":{
+            "name": "start",
+            "description": "text"
+        },
+    },
+    "initiated":{
+        "finalized":{
+            "name": "finalize",
+            "description": "text"
+        }
+    },
+}
