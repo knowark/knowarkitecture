aeff2f3ee8fe062f7ff1b9202053332bcabe6d8d
Esteban Echeverry <eecheverry@knowark.com>
2022-04-06 15:41:04 +0100

    Create contextor as a general instrument.

--- example/server/src/application/general/instruments/contextor.js
+++ example/server/src/application/general/instruments/contextor.js
@@ -1,3 +1,15 @@
-export class Contextor {
+import { AsyncLocalStorage } from 'async_hooks'
 
+export class Contextor {
+  constructor() {
+    this.storage = new AsyncLocalStorage()
+  }
+
+  context() {
+    return this.storage.getStore()
+  }
+
+  run(context, method) {
+    return this.storage.run(context, method)
+  }
 }
