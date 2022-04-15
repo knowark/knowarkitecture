bfb7d0f6ce02b665130d8c3a8ef32fdf5997e446
Esteban Echeverry <eecheverry@knowark.com>
2022-04-15 19:29:28 +0100

    Move model tests next to their runtime code.

---
+++ example/server/src/application/domain/models/teacher.spec.js
@@ -0,0 +1,34 @@
+import { describe, it, expect, beforeEach } from '@jest/globals'
+import { Teacher } from './teacher.js'
+
+describe('Teacher', () => {
+    let teacher = null
+
+    beforeEach(() => {
+        teacher = new Teacher({
+            id: 'T001',
+            name: 'Richard Roe',
+            identification: '67890',
+            email: 'rroe@example.com'
+        })
+    })
+    
+    it('can be instantiated', () => {
+        expect(teacher).toBeTruthy()
+    })
+
+    it('can define its attributes', () => {
+        expect(teacher.id).toEqual('T001')
+        expect(teacher.name).toEqual('Richard Roe')
+        expect(teacher.identification).toEqual('67890')
+        expect(teacher.email).toEqual('rroe@example.com')
+    })
+
+    it('defines default attributes', () => {
+        teacher = new Teacher()
+        expect(teacher.id.length > 0).toBeTruthy()
+        expect(teacher.name).toEqual('')
+        expect(teacher.identification).toEqual('')
+        expect(teacher.email).toEqual('')
+    })
+})
