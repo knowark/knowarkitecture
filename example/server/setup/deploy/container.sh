3e41c1fe2254ac8fd91c91f238c5807d8f9dbe69
Esleyder <knowarkbot@knowark.com>
2022-05-17 14:28:27 -0500

    node16 was integrated to base.profile.yml

--- example/server/setup/deploy/container.sh
+++ example/server/setup/deploy/container.sh
@@ -2,15 +2,6 @@
 
 CONTAINER=tutorark
 
-lxc launch ubuntu:20.04 $CONTAINER
-
-lxc config device add $CONTAINER config disk source=/home path=/mnt/home
-
-sleep 3
-
-lxc exec $CONTAINER -- /bin/bash -c "curl -fsSL \
-    https://deb.nodesource.com/setup_16.x | sudo -E bash -"
-
-lxc exec $CONTAINER -- apt update -y
-
-lxc exec $CONTAINER -- apt install -y build-essential nodejs
+lxc launch ubuntu:20.04 $CONTAINER -p base
+echo "Installing packages"
+lxc exec $CONTAINER -- cloud-init status --wait
